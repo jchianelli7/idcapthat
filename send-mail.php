2c4856e920a2ad8daa7c4eff9980f7394dc70cab
Josh Chianelli <joshchianelli@gmail.com>
2017-12-10 10:37:06 -0600

    Added spam filter to php mail

--- send-mail.php
+++ send-mail.php
@@ -1,5 +1,3 @@
-
-
 <?
 
 $name=$_POST['name'];
@@ -7,8 +5,13 @@ $Email=$_POST['email'];
 $website=$_POST['url'];
 $message=$_POST['message'];
 
+	
+    $SpamErrorMessage = "No Websites URLs permitted";
 
-    
+	if(preg_match("/http/i","$name"))(echo "$SpamErrorMessage"; exit())
+	if(preg_match("/http/i","$Email"))(echo "$SpamErrorMessage"; exit())
+	if(preg_match("/http/i","$message"))(echo "$SpamErrorMessage"; exit())
+				
     $body .= "Name: " . $name . "\n"; 
     $body .= "Email: " . $Email . "\n"; 
     $body .= "Website: " . $website . "\n"; 
@@ -19,10 +22,13 @@ $message=$_POST['message'];
   
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<script>alert("Your message has been sent successfully. We will contact you shortly.");</script>
-<meta HTTP-EQUIV="REFRESH" content="0; url=index.html"> 
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml">
 
-</head>
+	<head>
+		<script>
+			alert("Your message has been sent successfully. We will contact you shortly.");
+		</script>
+		<meta HTTP-EQUIV="REFRESH" content="0; url=index.html">
+
+	</head>
